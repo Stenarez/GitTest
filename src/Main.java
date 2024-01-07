99b095b109939afe4d28d6469b89e5f7f180bb96
Stenarez <stena@bk.ru>
2024-01-07 23:49:02 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+    public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum++;
+        }
+        System.out.println("i = " + sum);
+    }
+}
