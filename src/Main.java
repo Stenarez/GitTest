d2f0f02bb438b8a83e03c9198ca6fa7044991231
Stenarez <stena@bk.ru>
2024-01-08 13:44:39 +0300

    task1 -1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
     public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome Gus!  ");
+        System.out.println("Hello and welcome Gus-Bud!  ");
         int sum = 0;
         for (int i = 1; i <= 5; i++) {
             sum++;
