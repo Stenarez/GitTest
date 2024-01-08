90d4bc6dd9b6fd4b308bf082f260625f1bb01510
Stenarez <stena@bk.ru>
2024-01-08 15:33:59 +0300

    task2-1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
     public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello!  ");
         System.out.println("Hello Gus-Bus and welcome !!!  ");
         int sum = 0;
         for (int i = 1; i <= 7; i++) {
@@ -9,4 +9,5 @@
         System.out.println("i = " + sum);
         System.out.println("i*i = " + sum*sum);
     }
+
 }
